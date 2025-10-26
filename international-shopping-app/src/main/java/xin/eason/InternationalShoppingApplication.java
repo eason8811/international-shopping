87fe4473992baddfd41adde252b0ca0c842380e5
Eason <3220563569@qq.com>
2025-10-26 18:57:44 +0800

    初始化项目相关模块

---
+++ international-shopping-app/src/main/java/xin/eason/InternationalShoppingApplication.java
@@ -0,0 +1,11 @@
+package xin.eason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternationalShoppingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InternationalShoppingApplication.class, args);
+    }
+}
