e34ed9f47e8ae93e1857b00a5f7de7099e3c2c8d
Eason <3220563569@qq.com>
2025-10-28 18:08:56 +0800

    为枚举类添加详细注释

--- international-shopping-domain/src/main/java/shopping/international/domain/model/enums/user/AuthProvider.java
+++ international-shopping-domain/src/main/java/shopping/international/domain/model/enums/user/AuthProvider.java
@@ -3,7 +3,15 @@ package shopping.international.domain.model.enums.user;
 import lombok.Getter;
 
 /**
- * 第三方/本地认证通道，与表 user_auth.provider 一致。
+ * 第三方 / 本地认证通道, 与表 {@code user_auth.provider} 一致
+ * <ul>
+ *     <li>{@code LOCAL}: 本地认证</li>
+ *     <li>{@code GOOGLE}: Google 认证</li>
+ *     <li>{@code FACEBOOK}: Facebook 认证</li>
+ *     <li>{@code APPLE}: Apple 认证</li>
+ *     <li>{@code INSTAGRAM}: Instagram 认证</li>
+ *     <li>{@code TIKTOK}: TikTok 认证</li>
+ * </ul>
  */
 @Getter
 public enum AuthProvider {
