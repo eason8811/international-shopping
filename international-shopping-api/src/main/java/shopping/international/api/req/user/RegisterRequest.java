8cb3e2084df44a54f8f01a563c251fa55ddb049e
Eason <3220563569@qq.com>
2025-11-05 20:10:49 +0800

    fix: 修复了手机号在注册阶段必填的问题, 以及邮箱格式校验了两次并且两次的正则表达式不一致的问题

--- international-shopping-api/src/main/java/shopping/international/api/req/user/RegisterRequest.java
+++ international-shopping-api/src/main/java/shopping/international/api/req/user/RegisterRequest.java
@@ -5,8 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import shopping.international.types.exceptions.IllegalParamException;
 
-import java.util.regex.Pattern;
-
 import static shopping.international.types.utils.FieldValidateUtils.*;
 
 /**
@@ -38,12 +36,6 @@ public class RegisterRequest {
     private String phone;
 
     /**
-     * 匹配邮箱格式正则表达式
-     * 此模式主要用于 {@link RegisterRequest#validate()} 方法中, 匹配邮箱格式
-     */
-    private static final Pattern emailPattern = Pattern.compile("\\w+@\\w+(\\.\\w+)+");
-
-    /**
      * 验证请求 DTO 字段是否合法
      *
      * @throws IllegalParamException 如果任一被验证的字段不符合预期条件
@@ -53,7 +45,5 @@ public class RegisterRequest {
         requireNotBlank(password, "密码不能为空");
         requireNotBlank(nickname, "昵称不能为空");
         requireNotBlank(email, "邮箱不能为空");
-        require(emailPattern.matcher(email).matches(), "邮箱格式不正确");
-        requireNotNull(phone, "手机号码不能为 NULL");
     }
 }
