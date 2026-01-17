144ca12a9457fdbf8945d6a18b76f3cc19c9fee1
Eason <3220563569@qq.com>
2026-01-17 16:29:12 +0800

    change: 调整 order 表和 payment_order 表结构

--- international-shopping-domain/src/main/java/shopping/international/domain/model/enums/orders/PayStatus.java
+++ international-shopping-domain/src/main/java/shopping/international/domain/model/enums/orders/PayStatus.java
@@ -5,15 +5,19 @@ package shopping.international.domain.model.enums.orders;
  * <ul>
  *     <li>{@code NONE} - 无支付状态</li>
  *     <li>{@code INIT} - 初始化</li>
+ *     <li>{@code PENDING} - 正在支付</li>
  *     <li>{@code SUCCESS} - 支付成功</li>
  *     <li>{@code FAIL} - 支付失败</li>
  *     <li>{@code CLOSED} - 支付关闭</li>
+ *     <li>{@code EXCEPTION} - 异常</li>
  * </ul>
  */
 public enum PayStatus {
     NONE,
     INIT,
+    PENDING,
     SUCCESS,
     FAIL,
-    CLOSED
+    CLOSED,
+    EXCEPTION
 }
